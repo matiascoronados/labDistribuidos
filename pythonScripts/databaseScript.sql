6573e90b44b9dcd107714d77ee98d94c1bb65199
gabriel <gabriel.gonzalez.s@usach.cl>
2021-01-22 21:29:17 -0300

    CPythtonTQueryM

--- pythonScripts/databaseScript.sql
+++ pythonScripts/databaseScript.sql
@@ -8,16 +8,16 @@ DROP TABLE IF EXISTS comments;
 
 CREATE TABLE topics(
     id_topic SERIAL PRIMARY KEY,
-    topic_title VARCHAR(60) NOT NULL
+    topic_title VARCHAR(500) NOT NULL
 );
 
 CREATE TABLE posts(
     id_post VARCHAR(20) NOT NULL PRIMARY KEY,
     id_topic INT,
-    post_title VARCHAR(60) NOT NULL,
+    post_title VARCHAR(500) NOT NULL,
     post_author VARCHAR(30) NOT NULL,
     post_score INT NOT NULL,
-    post_link VARCHAR(60) NOT NULL,
+    post_link VARCHAR(200) NOT NULL,
     post_numComments INT NOT NULL,
     CONSTRAINT fk_topic
         FOREIGN KEY(id_topic) 
@@ -28,7 +28,7 @@ CREATE TABLE comments(
     id_comment VARCHAR(20) NOT NULL PRIMARY KEY,
     id_post VARCHAR(20),
     comment_author VARCHAR(30) NOT NULL,
-    comment_body VARCHAR(200) NOT NULL,
+    comment_body VARCHAR(600) NOT NULL,
     sentiment_value DECIMAL,
     CONSTRAINT fk_post
         FOREIGN KEY(id_post) 
--- pythonScripts/sentimentAnalysis.py
+++ pythonScripts/sentimentAnalysis.py
@@ -7,7 +7,7 @@ import nltk
 import psycopg2
 def analysisScore(text):
     #print(text['without_stops'])
-    print("comentario")
+    #print("comentario")
     blob = TextBlob(text['without_stops'])
     tag = blob.tags
     noun_phrases = blob.noun_phrases
@@ -23,10 +23,10 @@ def analysisScore(text):
     return promedio  
         
     
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('brown')
-client = MongoClient('34.68.42.248', username='admin', password='canito123', authSource='reddit', authMechanism='SCRAM-SHA-1')#datos de conexion de bd
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('brown')
+client = MongoClient('34.70.222.160', username='admin', password='canito123', authSource='reddit', authMechanism='SCRAM-SHA-1')#datos de conexion de bd
 #client = MongoClient('mongodb://riruriru:riruriru@35.239.147.51:27017/?authSource=admin&readPreference=primary&ssl=false')
 db = client["reddit"]#asi se accede a una bd
 collection = db["comments"]#asi se accede a una coleccion
@@ -39,29 +39,52 @@ conn.autocommit = True
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
-#cursor.execute("INSERT INTO topics(id_topic,topic_title) VALUES ('1','hola')")
-
-
-cursor.execute("SELECT %s FROM topics WHERE id_topic=%s",(1,2))
-a = cursor.fetchone()
-print(a)
-
 
 dataDB = collection.find()
 
+
+
+for comment in dataDB:
+
+
+    # Variables 
+
+    sentimentValue = analysisScore(comment['stats'])
+    aux=comment['post']['Title'].replace("'","")
+    cursor.execute("SELECT EXISTS(SELECT 1 FROM topics WHERE topic_title='"+aux+"')")
+    queryTopicExist = cursor.fetchone()[0]
+
+
+    if(queryTopicExist):
+        print("Si esta")
+
+        #cursor.execute("SELECT id_topic FROM topics WHERE topic_title='"+comment['subredditTitle']+"'")
+        #idTopic = cursor.fetchone()[0]
+        #print(idTopic)
+        
+
+    else:
+        print("No esta")
+
+        # Insertar Topico
+        cursor.execute("INSERT INTO topics(id_topic,topic_title) VALUES (DEFAULT,'"+comment['subredditTitle'].replace("'","")+"') RETURNING id_topic")
+        id_topic = cursor.fetchone()[0]
+        #print(id_topic)
+
+        aux=comment['post']['Title'].replace("'","")
+        aux=aux[0:500]
+        print(aux)
+        # Insertar Post
+        cursor.execute("""INSERT INTO posts (id_post, id_topic, post_title, post_author, post_score, post_link, post_numcomments) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s)""", 
+                (comment['post']['Id'], id_topic, aux, comment['post']['Author'], comment['post']['Score'], comment['post']['Link'], comment['post']['NumComments']))
+
+        # Insertar Comentario
+        cursor.execute("""INSERT INTO comments(id_comment, id_post, comment_author, comment_body, sentiment_value) 
+                VALUES (%s, %s, %s, %s, %s)""", 
+                (comment['post']['Id'], comment['post']['Id'], comment['commentAuthor'], comment['commentBody'][0:500], sentimentValue))
+
 cursor.close()
 conn.close()
-
-
-"""for comment in dataDB:
-    #print(comment)
-    sentimentValue = analysisScore(comment['stats'])
-
-    #a = cursor.execute("SELECT EXISTS(SELECT 1 FROM topics WHERE topic_title="+comment['subredditTitle']+")")
-    a = cursor.execute("SELECT 1 FROM topics WHERE id_topic=1")
-
-    print(a)"""
-    
-
 
 
