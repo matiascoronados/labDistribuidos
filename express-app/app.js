13428db6c8bf1044ced80a93bfd0f00c7b274b3f
Matias <matias.coronado@usach.cl>
2021-01-26 23:00:03 -0300

    CORS error fix + changes in package.json scripts

--- express-app/app.js
+++ express-app/app.js
@@ -18,6 +18,17 @@ app.use(
   })
 )
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 
 //routes
 app.use(require('./routes/routes'));
