887e1375786470f0e0ee22caabccd0e82798ba8b
Benjamín Muñoz <benjamin.munoz.tapia@gmail.com>
2021-02-01 23:38:15 -0300

    links

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,18 +4,17 @@
             <b-navbar-item tag="router-link" :to="{ path: '/' }">
                 <img
                     src="https://miro.medium.com/max/720/1*mQz1eSo1ZkL-Rufb5Xfrqw.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
                 >
             </b-navbar-item>
         </template>
         <template #start>
-            <b-navbar-item href="/">
+            <b-navbar-item router-link href="/">
                 Home
             </b-navbar-item>
-            <b-navbar-item href="/topics">
+            <b-navbar-item router-link href="/topics">
                 Topicos
             </b-navbar-item>
-            <b-navbar-item href="/posts">
+            <b-navbar-item router-link href="/posts">
                 Posts
             </b-navbar-item>
         </template>
